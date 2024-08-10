980e3b1bcc5a8755e2671ee0a5181cb9c40df05c
jpcodeplus <behrens.codeplus@gmail.com>
2024-08-10 21:34:33 +0200

    sets the charset of the html dynamically

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html <?php language_attributes();?>>
+<html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WCT One</title>
 </head>
