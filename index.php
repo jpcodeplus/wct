d2631b88e474c30577945cd49010dcc7f85e3d95
jpcodeplus <behrens.codeplus@gmail.com>
2024-08-10 21:36:07 +0200

    removed the title tag

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WCT One</title>
 </head>
 
 <body <?php body_class(); ?>>
